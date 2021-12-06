5534c58f6b04d42572e3aadbb39db5240d1b848d
pasarkarg <pasarkar.g@northeastern.edu>
2021-12-06 16:55:42 -0500

    Insert customer & mutual fund data sql queries

---
+++ Insert-Data_Creation_Queries.sql
@@ -0,0 +1,61 @@
+set define off;
+
+INSERT ALL
+    INTO CUSTOMER_DETAILS VALUES (11489 , 'Megan Joseph' , '19-jun-1980' , 41, 2347905511 , 92133 , 'New York' , '41, Melvin street' , '22-FEB-2020' , 'Knight@1' , 'Safforn' , 'Balling')
+    INTO CUSTOMER_DETAILS VALUES (34749 , 'Wane Fox' , '11-NOV-1992' , 31, 2347865588 , 37553 , 'FLORIDA' , '22, Park Street' , '17-MAY-2019' , 'Toyyy777' , 'Sanban*8' , 'Yussss')
+    INTO CUSTOMER_DETAILS VALUES (55631 , 'Tejas Bawankar' , '11-JUL-1994' , 27, 8983122563 , 44043, 'Texas' , '71, Pandey layout' , '09-NOV-2019' , 'Aditi@04' , 'Mastaa' , 'Chobugomu')
+    INTO CUSTOMER_DETAILS VALUES (76231 , 'Siddharth Savant' , '19-JUL-1996' , 25, 9559240344 , 89645, 'California' , '55, South Nagar' , '26-SEP-2017' , 'Cloud@04' , 'Funnyman' , 'Rowbow2')
+    INTO CUSTOMER_DETAILS VALUES (90214 , 'Vaibhavi T' , '09-Oct-1996' , 25, 8149935345 , 77564 , 'Texas' , '71, Effco Way' , '14-OCT-2018' , 'Space@05' , 'Cutiepie' , 'Unicorn')
+    INTO CUSTOMER_DETAILS VALUES (87452 , 'Jack Columbus' , '01-jan-1988' , 32, 8149976541 , 72123 , 'Washington' , '89, Treat Streat' , '19-Jul-2021' , 'yahoo&5' , 'Lassi' , 'Beatit')
+    INTO CUSTOMER_DETAILS VALUES (31000,'Anne Maria','02-Apr-1980',41,8576870012,02130,'Arizona','75 Tent Street','13-Jan-2021','Tina@123','tiger','pet')
+    INTO CUSTOMER_DETAILS VALUES (31002,'Bill Gates','04-Sep-1950',71,8547865532,02137,'New Mexico','94 Las Palmas RicoTower','05-Feb-2020','RIYA@me','travel','read')
+    INTO CUSTOMER_DETAILS VALUES (31003,'Hillary Clinton','05-Sep-1998',23,2347865539,67139,'Florida','51 Venus Ave','21-Jul-2017','Loik@hjk','love','friends')
+    INTO CUSTOMER_DETAILS VALUES (31004,'Gauri Pasarkar','10-Dec-1991',31,2347985511,92135,'New York','41 Melvin street','21-Feb-2020','ujki@wer','this','that')
+    INTO CUSTOMER_DETAILS VALUES (31005,'Sneha Chavan','10-Oct-1998',23,2347865578,37553,'Florida','22 Park Street','09-May-2019','Toyyy779','meera','mamacita')
+    INTO CUSTOMER_DETAILS VALUES (31006,'Ram Wajpe','05-Dec-2004',17,8983122565,74047,'Texas','71 Shivneri nagar','25-Nov-2020','Meer@tuu','school','college')
+    INTO CUSTOMER_DETAILS VALUES (31007,'Herika Koshti','06-Dec-1994',27,9759240344,86648,'California','58 North Nagar','28-Jun-2017','Cloud@04','florida','texas')
+    INTO CUSTOMER_DETAILS VALUES (31008,'Priyanka Singh','01-Feb-1965',56,7149935345,77597,'Texas','75 Alpha Street','03-Oct-2018','444@hjkk','america','india')
+    INTO CUSTOMER_DETAILS VALUES (31009,'Aarti Mehta','02-Mar-1977',44,8169976541,72233,'Washington','101 Harbour point','08-Jul-2021','Tvj@7899','mountain','tesla')
+    INTO CUSTOMER_DETAILS VALUES (13459,'Samual Adams','10-NOV-1993',28,6173456712,02120,'Massachussets','21, Barclay avenue','4-Jul-18','sam11','ponyback','sweater')
+    INTO CUSTOMER_DETAILS VALUES (43678,'Mary J','5-Dec-1990',31,8571234560,23412,'California','34, saint cakestreet ','16-Aug-17','23fgh','afgan','chair')
+    INTO CUSTOMER_DETAILS VALUES (13478,'Mihir Khanna','3-Jan-1985',36,7194567120,21001,'New York','45, baker street','1-Oct-17','yuper11','lamp','pen')
+    INTO CUSTOMER_DETAILS VALUES (45912,'Peter Parking','4-Feb-1980',41,6172347098,21304,'New York','45, marker corner street','12-Sep-13','walk123','switch','bottle')
+    INTO CUSTOMER_DETAILS VALUES (34157,'Manu Singh','27-Mar-1989',32,6178907222,51111,'Texas','67, yard hall street','22-Jan-13','456sd','sweater','lamp')
+    INTO CUSTOMER_DETAILS VALUES (14591,'Divya Sen','15-May-1999',22,3478902450,21334,'Texas','90,alps way','15-Feb-15','assdf','wall','spoon')
+    INTO CUSTOMER_DETAILS VALUES (23567,'Josie Blue','5-Dec-1990',31,8572341681,56001,'Virginia','567, wall mount road','12-Feb-18','josie12','garden','flag')
+    INTO CUSTOMER_DETAILS VALUES (72012,'Hose mathew','15-Jul-1992',29,6172345098,21333,'Washington','102, walking bistro way','18-Jun-15','san001','button','floor')
+    INTO CUSTOMER_DETAILS VALUES (20367,'Carrie Andres','24-May-1977',44,9071267000,60023,'Alaska','71,treat pathway ','16-Jan-20','laptop12','mouse','chain')
+    INTO CUSTOMER_DETAILS VALUES (11123,'Andrew Hanson','16-Jul-1950',71,9731230096,32201,'New Jersey','111, baker street ','10-Feb-14','gage456','mat','chakna')
+    INTO CUSTOMER_DETAILS VALUES (21000, 'Cristiano Ronaldo','05-FEB-1985',36,8578576969,21209,'Texas',' 34 Tremont Street','18-DEC-17', 'Cris123','striker','penalty')
+    INTO CUSTOMER_DETAILS VALUES (21001,'Kevin Owens','09-MAR-1987',34,9282920039,23099,'Massachussets','69, Mountain Street','29-MAR-16','Owns123','wall','lamp')
+    INTO CUSTOMER_DETAILS VALUES (21002, 'Kathy Mclerens', '09-APR-1987',34,2287882920,28892,'California','87, Google Street','30-MAR-16','kijuh','Laptop','Apple')
+    INTO CUSTOMER_DETAILS VALUES (21003, 'Hanny Osas','09-MAY-1998',23,2288292883,23829,'Illinois','98, Kreson pathway','28-FEB-12','Hanny167','Mango','Sauce')
+    INTO CUSTOMER_DETAILS VALUES (21004, 'Orland Sequal','10-OCT-1996',23,2663782883,21201,'Arizona','98, Amazon street', '29-MAY-20','haha981','Sun', 'Desert')
+    INTO CUSTOMER_DETAILS VALUES (21005,'Lionel Messi','10-OCT-1998',23,8837728248,98221,'Massachussets','34,Alphosus Street', '31-JAN-21','CrisNo2','GOAT','AlsoBest')
+    INTO CUSTOMER_DETAILS VALUES (21006,'Benjamin Cena','12-MAY-1974',50,3546738264,12131,'Washington','98, Hungtington Ave','23-SEP-16','hbb82','Feed','Instagram')
+    INTO CUSTOMER_DETAILS VALUES (21007,'Joseph Orton','12-JUN-1974',34,9917717713,31312,'Maryland','76, Solapur','15-SEP-19','nk82','Food','Pav Bhaji')
+    INTO CUSTOMER_DETAILS VALUES (21008,'Kane Williamson','03-JAN-1967',54,1772123781,13535,'Florida','87, The rock park','17-AUG-14','mbqi','Misal','Idli')
+    INTO CUSTOMER_DETAILS VALUES (21009,'MS Dhoni','03-FEB-1967',28,2663772899,35666,'North Carolina','34, Dou Lane','09-OCT-19','nns','Puranpoli','Dosa')
+    
+SELECT * FROM DUAL;
+
+INSERT ALL
+    INTO MUTUAL_FUND_DETAILS VALUES ('MU1002' , 'L&T Midcap-Growth' , 'Medium' , 100.1)
+    INTO MUTUAL_FUND_DETAILS VALUES ('MU1003' , 'Principal Emerging Bluechip Fund-Growth' , 'Low' , 181.2)
+    INTO MUTUAL_FUND_DETAILS VALUES ('MU1004' , 'Franklin Build India Fund-Growth' , 'High' , 64.01)
+    INTO MUTUAL_FUND_DETAILS VALUES ('MU1005' , 'IDFC Infrastructure Fund-Growth' , 'Medium' , 50)
+    INTO MUTUAL_FUND_DETAILS VALUES ('MU1006' , 'Aditya Birla Sun Life Small Cap Fund' , 'High' , 105.5)
+    INTO MUTUAL_FUND_DETAILS VALUES ('MU1007' , 'SBI Small Cap Fund' , 'Medium' , 120.3)
+    INTO MUTUAL_FUND_DETAILS VALUES ('MU1008' , 'IDFC Tax Advantage (ELSS) Fund' , 'Low' , 68.01)
+    INTO MUTUAL_FUND_DETAILS VALUES ('MU1009' , 'L&T Emerging Businesses Fund' , 'Low' , 70.76)
+    INTO MUTUAL_FUND_DETAILS VALUES ('MU1010' , 'DSP BlackRock Natural Fund' , 'Medium' , 130.7)
+    INTO MUTUAL_FUND_DETAILS VALUES ('MU1011' , 'ICICI Prudential Midcap 150 ETF' , 'High' , 117.4)
+    INTO MUTUAL_FUND_DETAILS VALUES ('MU1012' , 'Quant Small Cap Fund' , 'Low' , 84.32)
+    INTO MUTUAL_FUND_DETAILS VALUES ('MU1013' , 'Tata Digital India Fund' , 'High' , 175)
+    INTO MUTUAL_FUND_DETAILS VALUES ('MU1014' , 'Sundaram Select Micro Cap' , 'Medium' , 128.4)
+    INTO MUTUAL_FUND_DETAILS VALUES ('MU1015' , 'Kotak Small Cap Fund' , 'Low' , 54.02)
+    
+
+SELECT * FROM DUAL;
+
+
