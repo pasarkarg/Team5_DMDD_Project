192ef72f97153972a69a75756b9f4b108bdfb101
krishika <singh.kris@northeastern.edu>
2021-12-07 18:23:21 -0500

    INSERT_SAMPLE_DATA.sql

---
+++ INSERT_SAMPLE_DATA.sql
@@ -0,0 +1,5 @@
+
+----------------------------------------------CUSTOMER DATA-------------------------------------------------------
+
+execute insert1.insert_customer(11489 , 'Megan Joseph' , '19-jun-1980' , 41, 2347905511 , 92133 , 'New York' , '41, Melvin street' , '22-FEB-2020' , 'Knight@1' , 'Safforn' , 'Balling');
+execute insert1.insert_customer(34749 , 'Wane Fox' , '11-NOV-1992' , 31, 2347865588 , 37553 , 'FLORIDA' , '22, Park Street' , '17-MAY-2019' , 'Toyyy777' , 'Sanban*8' , 'Yussss');
