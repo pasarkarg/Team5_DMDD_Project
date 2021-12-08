ff66029f8073863790dfa803fa52fc2054f87a35
pasarkarg <pasarkar.g@northeastern.edu>
2021-12-07 23:06:15 -0500

    Mutual_fund_details data script

--- INSERT_SAMPLE_DATA.sql
+++ INSERT_SAMPLE_DATA.sql
@@ -6,3 +6,30 @@ execute insert1.insert_customer(34749 , 'Wane Fox' , '11-NOV-1992' , 31, 2347865
 execute insert1.insert_customer(55631 , 'Tejas Bawankar' , '11-JUL-1994' , 27, 8983122563 , 44043, 'Texas' , '71, Pandey layout' , '09-NOV-2019' , 'Aditi@04' , 'Mastaa' , 'Chobugomu');
 execute insert1.insert_customer (76231 , 'Siddharth Savant' , '19-JUL-1996' , 25, 9559240344 , 89645, 'California' , '55, South Nagar' , '26-SEP-2017' , 'Cloud@04' , 'Funnyman' , 'Rowbow2');
 execute insert1.insert_customer (90214 , 'Vaibhavi T' , '09-Oct-1996' , 25, 8149935345 , 77564 , 'Texas' , '71, Effco Way' , '14-OCT-2018' , 'Space@05' , 'Cutiepie' , 'Unicorn');
+
+
+
+--------------------------------------------MUTUAL FUND DETAILS------------------------------------------------
+execute insert1.insert_mutual_fund_details('MU1002' , 'L&T Midcap-Growth' , 'Medium' , 100.1);
+execute insert1.insert_mutual_fund_details('MU1003' , 'Principal Emerging Bluechip Fund-Growth' , 'Low' , 181.2);
+execute insert1.insert_mutual_fund_details('MU1004' , 'Franklin Build India Fund-Growth' , 'High' , 64.01);
+execute insert1.insert_mutual_fund_details('MU1005' , 'IDFC Infrastructure Fund-Growth' , 'Medium' , 50);
+execute insert1.insert_mutual_fund_details('MU1006' , 'Aditya Birla Sun Life Small Cap Fund' , 'High' , 105.5);
+execute insert1.insert_mutual_fund_details('MU1007' , 'SBI Small Cap Fund' , 'Medium' , 120.3);
+execute insert1.insert_mutual_fund_details('MU1008' , 'IDFC Tax Advantage (ELSS) Fund' , 'Low' , 68.01);
+execute insert1.insert_mutual_fund_details('MU1009' , 'L&T Emerging Businesses Fund' , 'Low' , 70.76);
+execute insert1.insert_mutual_fund_details('MU1010' , 'DSP BlackRock Natural Fund' , 'Medium' , 130.7);
+execute insert1.insert_mutual_fund_details('MU1011' , 'ICICI Prudential Midcap 150 ETF' , 'High' , 117.4);
+execute insert1.insert_mutual_fund_details('MU1012' , 'Quant Small Cap Fund' , 'Low' , 84.32);
+execute insert1.insert_mutual_fund_details('MU1013' , 'Tata Digital India Fund' , 'High' , 175);
+execute insert1.insert_mutual_fund_details('MU1014' , 'Sundaram Select Micro Cap' , 'Medium' , 128.4);
+execute insert1.insert_mutual_fund_details('MU1015' , 'Kotak Small Cap Fund' , 'Low' , 54.02);
+
+
+
+
+
+
+
+
+
