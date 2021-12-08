88d51bf1d6e5f354f961c3b5bd4b182bb5516c7a
pasarkarg <pasarkar.g@northeastern.edu>
2021-12-08 09:40:24 -0500

    Added customer data & modified mutual transaction data

--- INSERT_SAMPLE_DATA.sql
+++ INSERT_SAMPLE_DATA.sql
@@ -12,7 +12,37 @@ execute insert1.insert_customer(11070 , 'Ted Artist' , '23-Mar-95' , 32, 8149976
 execute insert1.insert_customer(87454 , 'TOAD GUPTA' , '23-Mar-00' , 32, 8149976541 , 72123 , 'Washington' ,'Faang', '89, Treat Streat' , '19-Jul-2021' , 'yahoo&5' , 'Lassi' , 'Beatit');
 execute insert1.insert_customer(66751 , 'HOLLY WITTON' , '11-Nov-90' , 32, 8149976541 , 72123 , 'Washington' ,'Mouth', '89, Treat Streat' , '19-Jul-2021' , 'yahoo&5' , 'Lassi' , 'Beatit');
 execute insert1.insert_customer(11489 , 'Megan Joseph' , '19-Jun-80' , 41, 2347905511 , 72123 , 'Washington' , 'Dallas', '89, Treat Streat' , '19-Jul-2021' , 'yahoo&5' , 'Lassi' , 'Beatit');
-
+execute insert1.insert_customer (31000,'Anne Maria','02-Apr-1980',41,8576870012,02130,'Arizona','75 Tent Street','13-Jan-2021','Tina@123','tiger','pet')
+execute insert1.insert_customer(31001,'Ted Mosby','03-Jun-1990',32,8576871655,41115,'New Jersey','34 Tremont Street','7-Aug-2019','Kitc@345','pune','mumbai')
+execute insert1.insert_customer(31002,'Bill Gates','04-Sep-1950',71,8547865532,02137,'New Mexico','94 Las Palmas RicoTower','05-Feb-2020','RIYA@me','travel','read')
+execute insert1.insert_customer(31003,'Hillary Clinton','05-Sep-1998',23,2347865539,67139,'Florida','51 Venus Ave','21-Jul-2017','Loik@hjk','love','friends')
+execute insert1.insert_customer (31004,'Gauri Pasarkar','10-Dec-1991',31,2347985511,92135,'New York','41 Melvin street','21-Feb-2020','ujki@wer','this','that')
+execute insert1.insert_customer(31005,'Sneha Chavan','10-Oct-1998',23,2347865578,37553,'Florida','22 Park Street','09-May-2019','Toyyy779','meera','mamacita')
+execute insert1.insert_customer (31006,'Ram Wajpe','05-Dec-2004',17,8983122565,74047,'Texas','71 Shivneri nagar','25-Nov-2020','Meer@tuu','school','college')
+execute insert1.insert_customer(31007,'Herika Koshti','06-Dec-1994',27,9759240344,86648,'California','58 North Nagar','28-Jun-2017','Cloud@04','florida','texas')
+execute insert1.insert_customer (31008,'Priyanka Singh','01-Feb-1965',56,7149935345,77597,'Texas','75 Alpha Street','03-Oct-2018','444@hjkk','america','india')
+execute insert1.insert_customer (31009,'Aarti Mehta','02-Mar-1977',44,8169976541,72233,'Washington','101 Harbour point','08-Jul-2021','Tvj@7899','mountain','tesla')
+execute insert1.insert_customer (13459,'Samual Adams','10-NOV-1993',28,6173456712,02120,'Massachussets','21, Barclay avenue','4-Jul-18','sam11','ponyback','sweater')
+execute insert1.insert_customer (43678,'Mary J','5-Dec-1990',31,8571234560,23412,'California','34, saint cakestreet ','16-Aug-17','23fgh','afgan','chair')
+execute insert1.insert_customer (13478,'Mihir Khanna','3-Jan-1985',36,7194567120,21001,'New York','45, baker street','1-Oct-17','yuper11','lamp','pen')
+execute insert1.insert_customer (45912,'Peter Parking','4-Feb-1980',41,6172347098,21304,'New York','45, marker corner street','12-Sep-13','walk123','switch','bottle')
+execute insert1.insert_customer (34157,'Manu Singh','27-Mar-1989',32,6178907222,51111,'Texas','67, yard hall street','22-Jan-13','456sd','sweater','lamp')
+execute insert1.insert_customer(14591,'Divya Sen','15-May-1999',22,3478902450,21334,'Texas','90,alps way','15-Feb-15','assdf','wall','spoon')
+execute insert1.insert_customer(23567,'Josie Blue','5-Dec-1990',31,8572341681,56001,'Virginia','567, wall mount road','12-Feb-18','josie12','garden','flag')
+execute insert1.insert_customer(72012,'Hose mathew','15-Jul-1992',29,6172345098,21333,'Washington','102, walking bistro way','18-Jun-15','san001','button','floor')
+execute insert1.insert_customer (20367,'Carrie Andres','24-May-1977',44,9071267000,60023,'Alaska','71,treat pathway ','16-Jan-20','laptop12','mouse','chain')
+execute insert1.insert_customer (11123,'Andrew Hanson','16-Jul-1950',71,9731230096,32201,'New Jersey','111, baker street ','10-Feb-14','gage456','mat','chakna')
+execute insert1.insert_customer (21000, 'Cristiano Ronaldo','05-FEB-1985',36,8578576969,21209,'Texas',' 34 Tremont Street','18-DEC-17', 'Cris123','striker','penalty')
+execute insert1.insert_customer(21001,'Kevin Owens','09-MAR-1987',34,9282920039,23099,'Massachussets','69, Mountain Street','29-MAR-16','Owns123','wall','lamp')
+execute insert1.insert_customer(21002, 'Kathy Mclerens', '09-APR-1987',34,2287882920,28892,'California','87, Google Street','30-MAR-16','kijuh','Laptop','Apple')
+execute insert1.insert_customer (21003, 'Hanny Osas','09-MAY-1998',23,2288292883,23829,'Illinois','98, Kreson pathway','28-FEB-12','Hanny167','Mango','Sauce')
+execute insert1.insert_customer (21004, 'Orland Sequal','10-OCT-1996',23,2663782883,21201,'Arizona','98, Amazon street', '29-MAY-20','haha981','Sun', 'Desert')
+execute insert1.insert_customer (21005,'Lionel Messi','10-OCT-1998',23,8837728248,98221,'Massachussets','34,Alphosus Street', '31-JAN-21','CrisNo2','GOAT','AlsoBest')
+execute insert1.insert_customer (21006,'Benjamin Cena','12-MAY-1974',50,3546738264,12131,'Washington','98, Hungtington Ave','23-SEP-16','hbb82','Feed','Instagram')
+execute insert1.insert_customer (21007,'Joseph Orton','12-JUN-1974',34,9917717713,31312,'Maryland','76, Solapur','15-SEP-19','nk82','Food','Pav Bhaji')
+execute insert1.insert_customer (21008,'Kane Williamson','03-JAN-1967',54,1772123781,13535,'Florida','87, The rock park','17-AUG-14','mbqi','Misal','Idli')
+execute insert1.insert_customer (21009,'MS Dhoni','03-FEB-1967',28,2663772899,35666,'North Carolina','34, Dou Lane','09-OCT-19','nns','Puranpoli','Dosa')
+    
 
 
 --------------------------------------------MUTUAL FUND DETAILS------------------------------------------------
@@ -34,41 +64,41 @@ execute insert1.insert_mutual_fund_details('MU1015' , 'Kotak Small Cap Fund' , '
 
 --------------------------------------------MUTUAL FUND TRANSACTION-----------------------------------------------
 
-execute insert1.insert_mutual_fund_transactions ('MUF0000','MU1002',	31000,'20-Jan-21',30,99.12,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0001','MU1002',	31000,'21-Jan-21',20,100,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0002','MU1002',	31000,'21-Jan-21',20,100,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0003','MU1002',	31000,'15-Feb-21',50,97.12,'S');
-execute insert1.insert_mutual_fund_transactions ('MUF0004','MU1002',	31000,'24-Mar-21',20,96.67,'S');
-execute insert1.insert_mutual_fund_transactions ('MUF0005','MU1003',	31001,'12-Feb-21',10,179.65,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0006','MU1003',	31001,'15-Mar-21',15,178,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0007','MU1003',	31001,'25-Apr-20',28,170.9,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0008','MU1003',	31001,'1-May-21',13,182.13,'S');
-execute insert1.insert_mutual_fund_transactions ('MUF0009','MU1003',	31001,'5-Jul-21',40,183.9,'S');
-execute insert1.insert_mutual_fund_transactions ('MUF0010','MU1004',	31002,'15-Feb-21',102,	60.1,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0011','MU1004',	31002,'25-Feb-21',102,	65.02,'S');
-execute insert1.insert_mutual_fund_transactions ('MUF0012','MU1004',	31002,'18-Jan-21',70,	63.12,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0013','MU1004',	31002,'11-Apr-21',70,	62.3,'S');
-execute insert1.insert_mutual_fund_transactions ('MUF0014','MU1005',	31002,'2-Mar-21',800,	45.12,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0015','MU1005',	31002,'5-May-21',100,	46.12,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0016','MU1005',	31002,'21-Jun-21',304,	48.3,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0017','MU1005',	31002,'3-Apr-21',900,	53.55,'S');
-execute insert1.insert_mutual_fund_transactions ('MUF0018','MU1005',	31002,'14-May-21',304,	58,'S');
-execute insert1.insert_mutual_fund_transactions ('MUF0019','MU1006',	31003,'20-Apr-21',55,	101.34,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0020','MU1006',	31003,'17-Sep-21',55,	106.12,'S');
-execute insert1.insert_mutual_fund_transactions ('MUF0021','MU1006',	31003,'10-Mar-21',105,	95.12,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0022','MU1006',	31003,'1-Dec-21',105,	107.67,'S');
-execute insert1.insert_mutual_fund_transactions ('MUF0023','MU1006',	31003,'22-Feb-21',203,	98.32,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0024','MU1006',	31003,'18-Oct-21',203,	108.21,'S');
-execute insert1.insert_mutual_fund_transactions ('MUF0025','MU1007',	31004,'10-May-21',400,	119.16,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0026','MU1007',	31004,'29-Jul-21',400,	139.34,'S');
-execute insert1.insert_mutual_fund_transactions ('MUF0027','MU1009',	31004,'16-Mar-21',205,	65.43,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0028','MU1009',	31004,'18-Jul-21',303,	59.76,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0029','MU1009',	31004,'26-May-21',508,	87.63,'S');
-execute insert1.insert_mutual_fund_transactions ('MUF0030','MU1014',	31004,'13-Feb-21',16,	122.1,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0031','MU1014',	31004,'18-Nov-21',30,	124.14,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0032','MU1014',	31004,'14-Oct-21',58,	127.09,'B');
-execute insert1.insert_mutual_fund_transactions ('MUF0033','MU1014',	31004,'18-Mar-21',16,	145.16,'S');
-execute insert1.insert_mutual_fund_transactions ('MUF0034','MU1014',	31004,'9-Dec-21',88,150.14,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1002',	31000,'20-Jan-21',30,99.12,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1002',	31000,'21-Jan-21',20,100,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1002',	31000,'21-Jan-21',20,100,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1002',	31000,'15-Feb-21',50,97.12,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1002',	31000,'24-Mar-21',20,96.67,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1003',	31001,'12-Feb-21',10,179.65,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1003',	31001,'15-Mar-21',15,178,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1003',	31001,'25-Apr-20',28,170.9,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1003',	31001,'1-May-21',13,182.13,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1003',	31001,'5-Jul-21',40,183.9,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1004',	31002,'15-Feb-21',102,	60.1,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1004',	31002,'25-Feb-21',102,	65.02,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1004',	31002,'18-Jan-21',70,	63.12,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1004',	31002,'11-Apr-21',70,	62.3,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1005',	31002,'2-Mar-21',800,	45.12,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1005',	31002,'5-May-21',100,	46.12,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1005',	31002,'21-Jun-21',304,	48.3,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1005',	31002,'3-Apr-21',900,	53.55,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1005',	31002,'14-May-21',304,	58,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval'MU1006',	31003,'20-Apr-21',55,	101.34,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1006',	31003,'17-Sep-21',55,	106.12,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1006',	31003,'10-Mar-21',105,	95.12,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1006',	31003,'1-Dec-21',105,	107.67,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1006',	31003,'22-Feb-21',203,	98.32,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1006',	31003,'18-Oct-21',203,	108.21,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1007',	31004,'10-May-21',400,	119.16,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1007',	31004,'29-Jul-21',400,	139.34,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1009',	31004,'16-Mar-21',205,	65.43,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1009',	31004,'18-Jul-21',303,	59.76,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1009',	31004,'26-May-21',508,	87.63,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1014',	31004,'13-Feb-21',16,	122.1,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1014',	31004,'18-Nov-21',30,	124.14,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1014',	31004,'14-Oct-21',58,	127.09,'B');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1014',	31004,'18-Mar-21',16,	145.16,'S');
+execute insert1.insert_mutual_fund_transactions (MT_SEQ.nextval,'MU1014',	31004,'9-Dec-21',88,150.14,'S');
 
 
 
